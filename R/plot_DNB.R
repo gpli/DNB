1f45541a979e2d882af5208797ed9051108c2ebe
Gaopeng Li <939918989@qq.com>
2019-04-08 13:34:46 +0800

    plot with ggplot

--- R/plot_DNB.R
+++ R/plot_DNB.R
@@ -2,41 +2,43 @@
 #'
 #' Plot DNB attibutes from data frame
 #' @param dnb_df a data frame of DNB result.
-#' @param file a character string of the output PDF file.
-#' @param height,width the width and height of the graphics region in inches.
+#' @param outfile a character string of the output image file.
+#' @param ... additional arguments to ggplot2::ggsave
 #' @export
-plot_DNB <- function(dnb_df, file="DNB_plot.pdf", height=7, width=7) {
-    pdf(file,height = height ,width = width)
-    layout(matrix(c(1:4),ncol=2))
-
+plot_DNB <- function(dnb_df, outfile = NULL, ...) {
     # score
-    plot(dnb_df$score,type="l",col = "blue", xaxt = "n" , xlab = "time", ylab = "score")
-    points(dnb_df$score,col = "black")
-    axis(1,1:nrow(dnb_df),round(dnb_df$time))
+    p_score <- plot_DNB_attr(dnb_df, "score")
 
     # cor_in
     if (!is.null(dnb_df$cor_in_ctrl)) {
         dnb_df$cor_in <- dnb_df$cor_in / dnb_df$cor_in_ctrl
     }
-    plot(dnb_df$cor_in, type="l", col = "blue", xaxt = "n" , xlab = "time", ylab = "cor_in")
-    points(dnb_df$cor_in,col = "black")
-    axis(1,1:nrow(dnb_df),round(dnb_df$time))
+    p_cor_in <- plot_DNB_attr(dnb_df, "cor_in")
 
     # cor_out
     if (!is.null(dnb_df$cor_out_ctrl)) {
         dnb_df$cor_out <- dnb_df$cor_out / dnb_df$cor_out_ctrl
     }
-    plot(dnb_df$cor_out, type="l", col = "blue", xaxt = "n" , xlab = "time", ylab = "cor_out")
-    points(dnb_df$cor_out,col = "black")
-    axis(1,1:nrow(dnb_df),round(dnb_df$time))
+    p_cor_out <- plot_DNB_attr(dnb_df, "cor_out")
 
     # cv
     if (!is.null(dnb_df$cv_in_ctrl)) {
         dnb_df$cv_in <- dnb_df$cv_in / dnb_df$cv_in_ctrl
     }
-    plot(dnb_df$cv_in, type="l", col = "blue", xaxt = "n" , xlab = "time", ylab = "CV")
-    points(dnb_df$cv_in,col = "black")
-    axis(1,1:nrow(dnb_df),round(dnb_df$time))
+    p_cv <- plot_DNB_attr(dnb_df, "cv_in")
 
-    dev.off()
+    p_merge <- cowplot::plot_grid(p_score, p_cor_in, p_cor_out, p_cv)
+    if (is.null(outfile)) {
+        return(p_merge)
+    } else {
+        ggplot2::ggsave(filename = outfile, plot = p_merge, ...)
+    }
 }
+
+plot_DNB_attr <- function(dnb_df, attr_name) {
+    ggplot2::ggplot(dnb_df, ggplot2::aes_string("time", attr_name)) +
+        ggplot2::geom_line(color = "blue") +
+        ggplot2::geom_point(shape = 21, size = 3) +
+        ggplot2::theme_classic()
+}
+
